a136ef2b30ae4a93c3cedbc1880251648dfc406e
ashutosh2407 <adwagh24@gmail.com>
2025-04-15 07:12:34 -0400

    Added the Group model

--- borrow/models.py
+++ borrow/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 # Create your models here.
 
 class Item(models.Model):
@@ -24,3 +24,20 @@ class Item(models.Model):
     def __str__(self):
         return self.item_name
 
+class Group(models.Model):
+    status = {
+        "A": "active",
+        "I": "inactive",
+        "D":"Dead"
+    }
+    group_id = models.BigAutoField(primary_key=True)
+    group_name = models.CharField(max_length=50, default=None, blank = False)
+    description = models.CharField(max_length=200, default=None)
+    city = models.CharField(max_length=20,default=None)
+    admin = models.ForeignKey(User, related_name="admin",null=True, on_delete=models.SET_NULL)
+    members = models.ManyToManyField(User)
+    time_created = models.DateTimeField(default = timezone.now())
+    status = models.CharField(choices = status, default = "A")
+
+    def __str__(self):
+        return f'{self.group_name} is in {self.status} state.'
