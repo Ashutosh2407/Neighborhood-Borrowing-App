0ac631ce45a3274a91602d9d28a9ca1b9d420d6a
ashutosh2407 <adwagh24@gmail.com>
2025-06-10 22:30:00 -0400

    Added extra kwargs for User and Item

--- borrow/serializers.py
+++ borrow/serializers.py
@@ -5,14 +5,18 @@ class UserSerizalizer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        extra_kwargs = {"password":{"write_only":True}}
 
 
 class ItemSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.PrimaryKeyRelatedField(queryset = User.objects.all())
     borrower = serializers.PrimaryKeyRelatedField(queryset = User.objects.all(), required = False)
+    
     class Meta:
         model = Item
         fields = ['url','item_id','item_name','owner','description','borrower','date_borrowed','due_date','created_at','status']
+        extra_kwargs = {"owner":{"read_only":True}}
+
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
