9731704dd94c5b3ab504ef6cd50b564d6bb8ce0b
ashutosh2407 <adwagh24@gmail.com>
2025-06-11 07:38:28 -0400

    Cleaned up urls

--- borrow/urls.py
+++ borrow/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = "index"),
+    path("home/", views.index, name = "index"),
     path("users/", views.user_list, name = "user_list"),
     path("user/<int:pk>/", views.user_detail, name = "user-detail"),
     path("items/", views.item_list, name = "item_list"),
