b0c6bc9c07727cf8c765a3a4d32b1993877adfd3
ashutosh2407 <adwagh24@gmail.com>
2025-06-18 07:32:11 -0400

    fixed dropdown menu click function in the profile pic in navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import Navbar from "../components/Navbar"
+
 function Home(){
   return(
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <>
+      <Navbar />
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg">
         <h1 className="text-3xl font-bold text-blue-600 mb-4">
           🎉 Tailwind CSS is working!
@@ -13,6 +17,8 @@ function Home(){
         </button>
       </div>
     </div>
+    </>
+    
   )
     
 }
