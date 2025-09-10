f6579d73ac5c3fe68523d19e68c4c6c0c1499ee7
ashutosh2407 <adwagh24@gmail.com>
2025-09-10 07:57:10 -0400

    Added 2 unit tests for User: 1)Creation 2) toString

---
+++ borrow/tests/test_user.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+# Create your tests here.
+class UserTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username = "User1", password= "abc123")
+
+    def test_instance_creation(self):
+        self.assertEqual(self.user.username, "User1")
+        self.assertTrue(self.user.check_password("abc123"))
+
+    def test_instance_tostring(self):
+        self.assertAlmostEqual(str(self.user), "User1")
+
+    
