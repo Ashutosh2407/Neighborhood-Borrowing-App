465d981ff153a1390b1960c8194b7104316b833b
ashutosh2407 <adwagh24@gmail.com>
2025-10-13 17:24:24 -0400

    added base frame for item list

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,14 @@
+import Itemfilter from "../components/Itemfilter"
 import Navbar from "../components/Navbar"
 
 function Home(){
   return(
     <>
       <Navbar />
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold text-blue-600 mb-4">
-          🎉 Tailwind CSS is working!
-        </h1>
-        <p className="text-gray-700 mb-4">
-          If you can see this styled properly, Tailwind is successfully installed.
-        </p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300">
-          Test Button
-        </button>
-      </div>
-    </div>
+      <Itemfilter />
+      
+      
+     
     </>
     
   )
